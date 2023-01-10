bdce230e99a036d6d98c4879aaabeea74d07f913
linx <slk1133@qq.com>
2023-01-10 13:54:32 +0800

    update log level

--- app/apinto/log.go
+++ app/apinto/log.go
@@ -10,7 +10,8 @@ func InitCLILog() {
 		TimestampFormat:  "2006-01-02 15:04:05",
 		CallerPrettyfier: nil,
 	}
-	transport := log.NewTransport(os.Stdout, log.InfoLevel)
+	log.SetLevel(log.DebugLevel)
+	transport := log.NewTransport(os.Stdout, log.DebugLevel)
 	transport.SetFormatter(formatter)
 	log.Reset(transport)
 }
